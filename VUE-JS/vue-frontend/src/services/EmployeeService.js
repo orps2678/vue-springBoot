69c946de57aaa658fac549051c331c54416e5f2f
TSUNG-YU WU <48641178+orps2678@users.noreply.github.com>
2023-07-21 13:11:46 +0800

    修改vue.js

--- VUE-JS/vue-frontend/src/services/EmployeeService.js
+++ VUE-JS/vue-frontend/src/services/EmployeeService.js
@@ -6,6 +6,11 @@ class EmployeeService{
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
+
+    // 新增 getEmployeeById 方法
+    getEmployeeById(id) {
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/${id}`);
+    }
 }
 
 export default new EmployeeService()
